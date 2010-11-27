8ee5b7c5020be0e3770922ba489db7be4d87d385
jannes.beurskens <jannes.beurskens@a3106b8e-1159-52ac-ecd4-90b8add11308>
2010-11-27 14:20:11 +0000

    Added picking up bombs and throwing them at point 0, 0.

--- Exercises/src/exerc1/BombRemovalBehaviour.java
+++ Exercises/src/exerc1/BombRemovalBehaviour.java
@@ -7,11 +7,9 @@ import java.util.Set;
 import gridworld.Environment;
 import gridworld.LogicalEnv;
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 
-public class BombRemovalBehaviour extends Behaviour{
-	private boolean _hasBomb;
-	private Point _targetBombLocation;
+public class BombRemovalBehaviour extends CyclicBehaviour{
 	private BombRemovalAgent _myAgent;
 	
 	public BombRemovalBehaviour(BombRemovalAgent owner)
@@ -19,59 +17,134 @@ public class BombRemovalBehaviour extends Behaviour{
 		_myAgent = owner;
 	}
 	
+	private void findNearestTrap()
+	{
+		// todo: find nearest
+		Iterator<Point> i = _myAgent.knownTraps.iterator();
+		while (i.hasNext())
+		{
+			_myAgent.targetTrapLocation = i.next();
+		}
+	}
+	
+	private void findNearestBomb(Set<Point> bombsFound)
+	{
+		Iterator<Point> i = bombsFound.iterator();
+		// todo: find nearest.
+		while(i.hasNext())
+		{
+			_myAgent.targetBombLocation = i.next();
+		}
+		System.out.println(String.format("%s: New target at (%s, %s)", _myAgent.getLocalName(), _myAgent.targetBombLocation.x, _myAgent.targetBombLocation.y));
+	}
+	
+	private void moveTowardsBombAndPickItUp()
+	{
+		// If target is on the target bomb pick it up.
+		if (_myAgent.currentLocation.x == _myAgent.targetBombLocation.x &&
+			_myAgent.currentLocation.y == _myAgent.targetBombLocation.y)
+		{
+			Environment.takeBomb(_myAgent.getLocalName());
+			// Set the target bomb location to null to indicate that there's
+			// no longer a current target.
+			_myAgent.targetBombLocation = null;
+			_myAgent.hasBomb = true;
+		}
+		// Otherwise move towards target bomb
+		else if (_myAgent.currentLocation.x > _myAgent.targetBombLocation.x)
+		{
+			Environment.west(myAgent.getLocalName());
+			_myAgent.currentLocation.x--;
+		}
+		else if (_myAgent.currentLocation.x < _myAgent.targetBombLocation.x)
+		{
+			Environment.east(myAgent.getLocalName());
+			_myAgent.currentLocation.x++;
+		}
+		else if (_myAgent.currentLocation.y > _myAgent.targetBombLocation.y)
+		{
+			Environment.north(myAgent.getLocalName());
+			_myAgent.currentLocation.y--;
+		}
+		else if (_myAgent.currentLocation.y < _myAgent.targetBombLocation.y)
+		{
+			Environment.south(myAgent.getLocalName());
+			_myAgent.currentLocation.y++;
+		}
+	}
+	
+	private void moveTowardsBinAndDropBomb()
+	{
+		// move towards trap to and drop the bomb into it.
+		// If target is on the target bomb pick it up.
+		if (_myAgent.currentLocation.x == _myAgent.targetTrapLocation.x &&
+			_myAgent.currentLocation.y == _myAgent.targetTrapLocation.y)
+		{
+			Environment.dropBomb(_myAgent.getLocalName());
+			// Set the target bomb location to null to indicate that there's
+			// no longer a current target.
+			_myAgent.targetTrapLocation = null;
+			_myAgent.hasBomb = false;
+		}
+		// Otherwise move towards target bomb
+		else if (_myAgent.currentLocation.x > _myAgent.targetTrapLocation.x)
+		{
+			Environment.west(myAgent.getLocalName());
+			_myAgent.currentLocation.x--;
+		}
+		else if (_myAgent.currentLocation.x < _myAgent.targetTrapLocation.x)
+		{
+			Environment.east(myAgent.getLocalName());
+			_myAgent.currentLocation.x++;
+		}
+		else if (_myAgent.currentLocation.y > _myAgent.targetTrapLocation.y)
+		{
+			Environment.north(myAgent.getLocalName());
+			_myAgent.currentLocation.y--;
+		}
+		else if (_myAgent.currentLocation.y < _myAgent.targetTrapLocation.y)
+		{
+			Environment.south(myAgent.getLocalName());
+			_myAgent.currentLocation.y++;
+		}
+	}
+	
 	@Override
 	public void action() {
-		if (_targetBombLocation == null)
+		if (_myAgent.targetBombLocation == null)
 		{
-			// get new target bomb.
-			Set<Point> bombsFound = LogicalEnv.getEnv().senseBombs(_myAgent.getLocalName());
-	
-			if (bombsFound.size() > 0)
+			if (_myAgent.hasBomb)
 			{
-				Iterator<Point> i = bombsFound.iterator();
-				// find nearest bomb.
-				while(i.hasNext())
+				if (_myAgent.targetTrapLocation == null)
 				{
-					_targetBombLocation = i.next();
+					// if there's no trap location find one
+					findNearestTrap();
 				}
-				System.out.println(String.format("%s: New target at (%s, %s)", _myAgent.getLocalName(), _targetBombLocation.x, _targetBombLocation.y));
+				else
+				{
+					moveTowardsBinAndDropBomb();
+				}
 			}
 			else
 			{
-				// Move around the environment to find bombs.
+				// get a list of possible target bombs.
+				// todo: make this a ticker thingie, so it won't be spammed if he can't find
+				// bombs.
+				Set<Point> bombsFound = Environment.senseBombs(_myAgent.getLocalName());
+		
+				if (bombsFound.size() > 0)
+				{
+					findNearestBomb(bombsFound);
+				}
+				else
+				{
+					// todo: Move around the environment to find bombs.
+				}
 			}
 		}
 		else
 		{
-			// Move towards target bomb
-			if (_myAgent.currentLocation.x > _targetBombLocation.x)
-			{
-				Environment.west(myAgent.getLocalName());
-				_myAgent.currentLocation.x--;
-			}
-			else if (_myAgent.currentLocation.x < _targetBombLocation.x)
-			{
-				Environment.east(myAgent.getLocalName());
-				_myAgent.currentLocation.x++;
-			}
-			else if (_myAgent.currentLocation.y > _targetBombLocation.y)
-			{
-				Environment.north(myAgent.getLocalName());
-				_myAgent.currentLocation.y--;
-			}
-			else if (_myAgent.currentLocation.y < _targetBombLocation.y)
-			{
-				Environment.south(myAgent.getLocalName());
-				_myAgent.currentLocation.y++;
-			}
+			moveTowardsBombAndPickItUp();
 		}
-		
-		//System.out.println(_myAgent.getLocalName() + ": " + bombsFound.size() + " bombs found.");
-	}
-
-	@Override
-	public boolean done() {
-		// TODO Auto-generated method stub
-		return _hasBomb;
 	}
 }
