3443987d9775ca3c3064fd2881e1238d316e9632
jannes.beurskens <jannes.beurskens@a3106b8e-1159-52ac-ecd4-90b8add11308>
2010-11-27 14:20:11 +0000

    Added picking up bombs and throwing them at point 0, 0.

--- trunk/Exercises/src/exerc1/BombRemovalAgent.java
+++ trunk/Exercises/src/exerc1/BombRemovalAgent.java
@@ -1,18 +1,30 @@
 package exerc1;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 import gridworld.Environment;
 import jade.core.Agent;
 
 public class BombRemovalAgent extends Agent {
 	public Point currentLocation;
+	public boolean hasBomb;
+	public Point targetBombLocation;
+	public Point targetTrapLocation;
+	public ArrayList<Point> knownTraps;
 	
 	protected void setup()
 	{
 		addBehaviour(new BombRemovalBehaviour(this));
-		// todo: get starting location from arguments.
+		// todo: get starting and trap locations from arguments.
 		currentLocation = new Point(5, 5);
+		knownTraps = new ArrayList<Point>();
+		knownTraps.add(new Point(0, 0));
+
 		Environment.enter(getLocalName(), currentLocation, "blue");
+		
 		System.out.println(getLocalName() + " is ready.");
 	}
 }
