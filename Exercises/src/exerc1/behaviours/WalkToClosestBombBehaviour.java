cc3098b806674435bffe571b102d104909cfe1ac
fmeeuwen <fmeeuwen@a3106b8e-1159-52ac-ecd4-90b8add11308>
2010-12-03 11:15:51 +0000

    WalkToClosestBomb behaviour works now

--- Exercises/src/exerc1/behaviours/WalkToClosestBombBehaviour.java
+++ Exercises/src/exerc1/behaviours/WalkToClosestBombBehaviour.java
@@ -27,19 +27,26 @@ public class WalkToClosestBombBehaviour extends SimpleBehaviour
 		System.out.println("In WalkToClosestBombBehaviour action");
 		
 		if(!_owner.knowsBombs())
+		{
+			System.out.println("Agent doenst know bombs. returning.");
 			return;
+		}
 		
 		///If its the first time this behaviour is used, then the step is not performed.
 		///In that case only the Sensing is performed, so always the area is first sensed and then walke
 		if(!_firstTimeUse)
 		{
+			System.out.println("2nd time use..");
 			
 			Point currentPosition = _owner.getCurrentPosition();
 			Set<Point> knownBombs = _owner.getKnownBombs();
 			
+			System.out.println("2nd time use.. positions retrived");
 			Point minimalDistanceBomb = null;
 			double minimalDistance = Double.MAX_VALUE;
-				
+			
+			System.out.println("Going to loop..");
+			
 			for (Point bombPosition : knownBombs)
 			{
 				double distance = bombPosition.distance(currentPosition);
@@ -64,25 +71,45 @@ public class WalkToClosestBombBehaviour extends SimpleBehaviour
 	@Override
 	public boolean done() 
 	{
+		System.out.println("in WalkToClosestBombBehaviour DONE");
+		
+		
 		if(!_owner.knowsBombs())
 		{
 			System.out.println("Agent doenst known bomb quiting!");
 			return true;
 		}
-		else
-		{	
-			for(Point bombPosition : _owner.getKnownBombs())
+		System.out.println("Agent does know at leat 1 bomb!!");
+		
+		System.out.println("Going to loop through known bombs");
+		
+		for(Point bombPosition : _owner.getKnownBombs())
+		{
+			System.out.println("In the loop.");
+			if(_owner.getCurrentPosition() != null)
 			{
 				if (bombPosition.equals(_owner.getCurrentPosition()))
 				{
 					System.out.println("Agent is on bomb.. quiting!");
 					return true;
 				}
+				else
+				{
+					System.out.println("Agent is not on bomb ... continuing.");
+				}
 			}
+			else
+			{
+				System.out.println("Agent current position is null so not on bomb.");
+				return true;
+			}	
 			
-			System.out.println("Agent not on bomb returning false");
-			return false;
 		}
+		
+		
+		System.out.println("Agent not on bomb returning false");
+		return false;
+		
 	}
 	
 	
